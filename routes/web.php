3c5b26d490ea460d58ec69bb8885a161fd8463e3
usamaali676 <alex1616jone@gmail.com>
2024-10-16 02:27:55 +0500

    cpanel integration

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,13 @@ use App\Http\Middleware\PermissionMiddelware;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::controller(FrontController::class)
 ->prefix('front')
