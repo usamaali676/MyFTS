2e86f437ad3b58d931cf52b776d244b19c8c9a74
usamaali676 <alex1616jone@gmail.com>
2026-03-05 02:52:35 +0500

    route cache

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,18 @@ use App\Http\Controllers\UserController;
 use App\Http\Middleware\PermissionMiddelware;
 use App\Models\InvoiceServiceCharges;
 use App\Models\ServiceArea;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::get('/config-cache', function() {
+     $exitCode = Artisan::call('config:cache');
+    // $exitCodes = Artisan::call('route:cache');
+     return 'Config cache cleared';
+ });
 
 Auth::routes();
 
